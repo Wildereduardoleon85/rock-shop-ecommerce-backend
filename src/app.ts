de3db7c70008b6b026f2d4cdf9dfbbe3b6df539d
Wilder <wilder_eduardo85@hotmail.com>
2023-08-11 10:14:36 -0400

    feat: Add route for all products and single product

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+/* eslint-disable no-underscore-dangle */
 import express from 'express'
+import products from './data'
 require('dotenv').config()
 
 const app = express()
@@ -8,8 +10,17 @@ const port: number | string = process.env.PORT ?? 5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/', () => {
-  console.log('hello world')
+app.get('/health-check', (req, res) => {
+  res.send('OK')
+})
+
+app.get('/api/v1/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/v1/products/:id', (req, res) => {
+  const product = products.find((singleProduct) => singleProduct._id === req.params.id)
+  res.json(product)
 })
 
 app.listen(port, () => console.log(`server running on port ${port}`))
